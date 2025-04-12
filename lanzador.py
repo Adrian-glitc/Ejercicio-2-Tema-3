5884169d3c48e8fc66edd9bd4f9043fa2c9ee64a
Adrian-glitc <amadridm@student.universidadunie.com>
2025-04-12 13:02:58 +0200

    cambios

---
+++ lanzador.py
@@ -0,0 +1,20 @@
+from Matriz_Base import MatrizBase
+from MatrizRecursiva import MatrizRecursiva
+from MatrizIterativa import MatrizIterativa
+
+# Ejemplo de uso
+def main():
+    matriz = [
+        [2, 3, 1],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    matriz_obj = MatrizBase(matriz)
+    
+    matriz_recursiva = MatrizRecursiva(matriz)
+    print("Determinante (recursivo):", matriz_recursiva.determinante_recursivo())
+
+    # Usando MatrizIterativa
+    matriz_iterativa = MatrizIterativa(matriz)
+    print("Determinante (iterativo):", matriz_iterativa.determinante_iterativo())
--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-from Matriz_Base import MatrizBase
-from MatrizRecursiva import MatrizRecursiva
-from MatrizIterativa import MatrizIterativa
+from lanzador import main
 
-# Ejemplo de uso
 if __name__ == "__main__":
-    matriz = [
-        [2, 3, 1],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-
-    matriz_obj = MatrizBase(matriz)
-    
-    matriz_recursiva = MatrizRecursiva(matriz)
-    print("Determinante (recursivo):", matriz_recursiva.determinante_recursivo())
-
-    # Usando MatrizIterativa
-    matriz_iterativa = MatrizIterativa(matriz)
-    print("Determinante (iterativo):", matriz_iterativa.determinante_iterativo())
+    main()
